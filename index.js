7df8e920750fa0aa6fcfb62ef54c41e4aeec516e
Jahidul Islam <56561463+sammijahidul@users.noreply.github.com>
2023-10-21 01:16:31 +0600

    created simple crud method using prisma & express

--- index.js
+++ index.js
@@ -1,8 +1,88 @@
 import express from 'express';
-
 const app = express();
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 app.use(express.json());
+
+// Create a user api for sending data to database
+app.post("/api/v1/user", async (req, res) => {
+    try {
+        const { firstName, lastName, age } = req.body;
+        const newUser = await prisma.User.create({ data: req.body })
+        res.status(201).json({
+            status: "success",
+            data: newUser,
+        })              
+    } 
+    catch (error) {
+        res.status(500).json({
+            status: "failed",
+            message: "Something went wrong while creating user"
+        })        
+    }
+});
+
+// Get request api for fetching data from database
+app.get("/api/v1/users", async (req, res) => {
+    try {
+        const allUsers = await prisma.User.findMany();
+        res.status(200).json({
+            status: "success",
+            data: allUsers,
+        })       
+    } 
+    catch (error) {
+        res.status(500).json({
+            status: "failed",
+            message: "Something went wrong while getting data"
+        })
+    }
+});
+
+// Update a user api for sending updated data to database
+app.patch("/api/v1/user/:id", async (req,res) => {
+    try {
+        const id = req.params.id;
+        const {firstName, lastName, age} = req.body;
+        const updatedUser = await prisma.User.update({ 
+            where: {id: parseInt(id)}, 
+            data: req.body
+        });
+        res.status(201).json({
+            status: "success",
+            data: updatedUser,
+        })                     
+    }  
+    catch (error) {
+        res.status(500).json({
+            status: "failed",
+            message: "Something went wrong while updating user"
+        })        
+    }
+});
+
+// Delete a user api for deleting user data from database
+app.delete("/api/v1/user/remove/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const deleteUser = await prisma.User.delete({
+            where : {
+                id: parseInt(id)
+            }
+        })
+        res.status(200).json({
+            status: "successful",
+            data: deleteUser,
+        })       
+    } 
+    catch (error) {
+        res.status(500).json({
+            status: "failed",
+            message: "Something went wrong while updating user"
+        })                
+    }
+})
 
 app.listen(3001, ()=> {
     console.log(`Server is listening at http://localhost:${3001}`);
